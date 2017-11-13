d541e85d72d06cc909b4005e4428c920d49b07bc
Yael_Amaya <yiap2610@gmail.com>
2017-11-13 13:19:06 -0700

    Added a color button you can spam to kill someone with epilepsy. (But you know... don't do it, I'm not liable for your stupidity.)

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -7,19 +7,23 @@ import javax.swing.JFrame;
 public class GUIFrame extends JFrame
 {
 	private GUIAppController appController;
+	private GUIPanel appPanel;
 	
 	public GUIFrame(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
+		this.appPanel = new GUIPanel(appController);
+		
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);
 		this.setTitle("Calvin is a terrible person, he eats hobbits for lunch on a daily basis");
 		this.setSize(500, 500);
-		
+		this.setResizable(false);
 		this.setVisible(true);
 	}
 	
--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -6,13 +6,68 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.SpringLayout;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
+	private JButton firstButton;
+	private SpringLayout appLayout;
 	
 	public GUIPanel(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
+		firstButton = new JButton("touch the butt, I am the butt");
+		appLayout = new SpringLayout();
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
+	
+	/**
+	 * Used to add all components to the screen and install a layout manager.
+	 * Also used to set color. :D
+	 */
+	private void setupPanel()
+	{
+		this.setBackground(Color.cyan);
+		this.setLayout(appLayout);
+		this.add(firstButton);
+	}
+	
+	/**
+	 * Used to hold the constraint positioning fo the SpringLayout
+	 * AKA a gaint dumping ground for all the xxxx.putConstraint(....) lines.
+	 */
+	private void setupLayout()
+	{
+		appLayout.putConstraint(SpringLayout.NORTH, firstButton, 130, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.WEST, firstButton, 118, SpringLayout.WEST, this);
+	}
+	
+	private void setupListeners()
+	{
+		firstButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				changeScreenColor();
+			}
+		});
+	}
+	
+	private void changeScreenColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		
+		this.setBackground(new Color(red, green, blue));
+	}
+	
 }
